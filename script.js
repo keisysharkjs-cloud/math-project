5ed09e58859556620599ebcd2dc51153928d8612
ArsenyaProg <arsenya158@gmail.com>
2026-07-20 14:15:56 +0300

    fix JS

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const button = document.getElementById('facts__btn');
-const message = document.getElementById('facts__msg');
+const button = document.querySelector('#facts__btn');
+const message = document.querySelector('.facts__container');
 
-button.onclick = function() {
-  message.innerHTML = "Шифр RSA защищает пароли, основываясь на простых числах.<br><br>" +
-  "GPS рассчитывает положение по формуле расстояния.<br><br>" +
-  "Правило 72: вклад удвоится за 72 / процентную ставку лет.<br><br>" +
-  "Первый алгоритм написала Ада Лавлейс в 1843 году." +
-  "<p><i>Все эти факты — о математике, которая работает прямо сейчас, пока вы читаете этот сайт</i><p>";
-};
+function handleClick () {
+  message.classList.toggle('active');
+}
+
+button.addEventListener('click', handleClick);
